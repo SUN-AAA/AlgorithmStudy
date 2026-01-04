8c751839c87dbb5d3dce8ae89fbd3c0033f8c3c0
SUN-AAA <seouna06@gmail.com>
2026-01-04 19:27:48 +0900

    2231 피드백 반영

--- 기본문제/2231.py
+++ 기본문제/2231.py
@@ -1,5 +1,6 @@
 #피드백: while문을 이용하면 깨끗한 코드를 짤 수 있음. 지금은 너무 더티코드ㅎㅋ
 
+"""
 n = int(input())
 cnt = 0
 for i in range(1,n):
@@ -7,6 +8,28 @@ for i in range(1,n):
     if sum == n:
         print(i)
         cnt += 1
+        break
+
+if cnt == 0:
+    print(0)
+"""
+
+#피드백 반영
+#while문을 이용해 i % 10을 구하고 i = i // 10으로 초기화하는 작업을 i == 0이 될때까지 반복하면 가능
+
+n = int(input())
+cnt = 0
+
+for i in range(1,n):
+    temp = i
+    sum = i
+    while i != 0:
+        sum += i % 10
+        i = i // 10
+    
+    if sum == n:
+        cnt += 1
+        print(temp)
         break
 
 if cnt == 0:
