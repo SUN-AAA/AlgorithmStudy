2f8fba78115347249356e11ee2afc3277d8ade96
SUN-AAA <seouna06@gmail.com>
2026-01-13 00:04:22 +0900

    다리를 지나는 트럭

---
+++ 스택&큐/다리를 지나는 트럭.py
@@ -0,0 +1,34 @@
+#최대 대수, 최대 무게 주어짐
+#다리 길이 크기의 큐를 만듦 -> 한 대 추가될 때마다 pop(0), append(truck_weights.pop(0))
+#이때 만약 전체 무게 > 최대 무게 이라면 pop(0), append(0) (다리 위 최대 트럭 수는 current 배열 크기가 설정되어있어 초과될 일이 없음)
+#pop 될때마다 answer(시간) += 1
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    current = [0] * bridge_length
+    w = 0
+    while truck_weights or w > 0:
+        if truck_weights:
+            
+            end_p = current.pop(0)
+            w -= end_p
+            w += truck_weights[0]
+
+            if w <= weight:
+                temp = truck_weights.pop(0)
+                current.append(temp)
+                answer += 1
+            else:
+                w -= truck_weights[0]
+                current.append(0)
+                answer += 1
+
+        else:
+            answer += bridge_length
+            break
+    return answer
+
+print(solution(2,10,[7,4,5,6]))
+print(solution(100,100,[10]))
+print(solution(100,100,[10,10,10,10,10,10,10,10,10,10]))
+print(solution(1,5,[1]))
